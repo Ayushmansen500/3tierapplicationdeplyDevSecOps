ad72aee90c84d79c40cdc966b9457cf62ecf8f71
Ayushman Sen <ayushmansen500@gmail.com>
2025-12-08 15:48:13 +0530

    Update 1st-Jenkins.sh

--- 1st-Jenkins.sh
+++ 1st-Jenkins.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
+set -e
 
-# Install OpenJDK 17 JRE Headless
-sudo apt install openjdk-17-jre-headless -y
+sudo apt update -y
+sudo apt install -y fontconfig openjdk-17-jdk curl gnupg
 
-# Download Jenkins GPG key
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key \
+  | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
-# Add Jenkins repository to package manager sources
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" \
+  | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 
-# Update package manager repositories
-sudo apt-get update
+sudo apt update -y
+sudo apt install -y jenkins
 
-# Install Jenkins
-sudo apt-get install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
